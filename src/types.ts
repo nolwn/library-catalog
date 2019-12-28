b6f647730add861584ee8e6052ca0ce36595d46d
nolanee <nolanee@umich.edu>
2019-12-27 20:28:44 -0800

    added book interface

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+interface Book {
+    title: string;
+    author_first?: string;
+    author_last: string;
+
+    edition?: string;
+    place?: string;
+    publisher?: string;
+    date?: string;
+
+    isbn10?: string;
+    isbn13?: string;
+    LOC?: string;
+}

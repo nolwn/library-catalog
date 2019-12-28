7a73a4165b8ce25f3b4a4295ab77321f84e5bef2
Nolan <nolan.hellyer@gmail.com>
2019-12-27 20:04:38 -0800

    Added linter.

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -1,17 +1,14 @@
 import * as express from "express";
 import * as models from "../models/books";
 
-export const getBooks: express.RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+export const getBooks: express.RequestHandler = (req: express.Request, res: express.Response): void => {
     try {
         const books: string[] = models.getBooks();
         if (!books) {
             res.status(404);
         }
-        
         res.status(200).send(books);
     } catch(err) {
         res.status(500).send();
     }
-
-    
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { bookRouter } from "./routes/books";
 // import bodyParser from "body-parser";
 
-const app = express();
-const port = 3000;
+const app: express.Express = express();
+const port: number = 3000;
 
 // app.use(bodyParser);
 app.use("/books", bookRouter);

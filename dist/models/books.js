171378e59472e19e5611ba0e2654b19f461c3b7d
Nolan <nolan.hellyer@gmail.com>
2019-12-27 19:35:14 -0800

    Fixed gitignore and added routes to app.

--- dist/models/books.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBooks = function () {
-    return ["Moby Dick", "Jane Eyre", "20,000 Leagues Under the Sea"];
-};
-//# sourceMappingURL=books.js.map

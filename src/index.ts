0e9b2b1939e7149ec3ddd3c48e4b7d25ba034121
nolanee <nolanee@umich.edu>
2019-12-28 12:58:23 -0800

    fix merge conflict

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/books", bookRouter);
 
-<<<<<<< HEAD
-app.listen(port);
-=======
 app.listen(port, () => { console.log(`****---->>>> Listening on Port: ${port} <<<<----****`); });
->>>>>>> da13560ac18e160458bd7d452e1f97d1718b4689
